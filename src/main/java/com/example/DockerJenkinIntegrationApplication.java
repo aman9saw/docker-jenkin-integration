c76f5045224b0f38c69f8993b9a52efae7061308
aman9saw <sawaman9@gmail.com>
2022-04-22 21:56:19 +0530

    adding logger

--- src/main/java/com/example/DockerJenkinIntegrationApplication.java
+++ src/main/java/com/example/DockerJenkinIntegrationApplication.java
@@ -1,12 +1,21 @@
 package com.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DockerJenkinIntegrationApplication {
 
+	static Logger log = LoggerFactory.getLogger(DockerJenkinIntegrationApplication.class);
+	
+	public void init() {
+		log.info("Application started ........................");
+	}
+	
 	public static void main(String[] args) {
+		log.info("Application executed ........................");
 		SpringApplication.run(DockerJenkinIntegrationApplication.class, args);
 	}
 
